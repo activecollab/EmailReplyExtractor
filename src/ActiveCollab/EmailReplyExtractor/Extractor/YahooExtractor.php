a61d4b41a490f2d991859712dff5aeaad82a186a
Djordje Bakic <djordje.bakic@activecollab.com>
2015-10-16 15:22:55 +0200

    Remove signature and fix new line

--- src/ActiveCollab/EmailReplyExtractor/Extractor/YahooExtractor.php
+++ src/ActiveCollab/EmailReplyExtractor/Extractor/YahooExtractor.php
@@ -17,4 +17,12 @@
         '/On(.*?)wrote\:(.*?)/is'
       ]);
     }
+
+    static function toPlainText($html)
+    {
+      $html = str_replace('span', 'p', $html);
+      $html = preg_replace('/<div class="signature".+<\/div>/','', $html);
+      return parent::toPlainText($html);
+    }
+
   }
